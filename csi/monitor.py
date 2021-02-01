11040b794d93405a7582d0dd7e4857b184cade5d
Benjamin <benjamin.lesage@york.ac.uk>
2021-02-01 09:33:24 +0000

    Prototype refactored safety monitors

--- csi/monitor.py
+++ csi/monitor.py
@@ -4,7 +4,8 @@ import attr
 import funcy
 from lenses import bind
 from traces import TimeSeries
-from typing import Any, Callable, List, Mapping, Optional, Tuple, Union, Iterable, Set, TypeVar, FrozenSet
+from typing import Any, Callable, List, Mapping, Optional, Tuple, Union, Iterable, Set, TypeVar, FrozenSet, Dict, \
+    MutableMapping
 
 from csi.safety import Atom, Node
 
@@ -65,70 +66,75 @@ class Monitor:
     def __or__(self, other: Monitor) -> Monitor:
         return Monitor(self.conditions | other.conditions)
 
-    def atoms(self) -> Iterable[Atom]:
-        return bind(self.conditions).F(lambda c: c.walk()).Instance(Atom).collect()
+    def atoms(self, condition=None) -> Iterable[Atom]:
+        return bind(self.conditions if condition is None else {condition}).F(lambda c: c.walk()).Instance(Atom).collect()
 
-    def evaluate(self, trace: Trace, condition: Optional[Node]) -> Mapping[Node, Optional[bool]]:
-        pass
+    def evaluate(self, trace: Trace, condition: Optional[Node] = None, *, dt=1.0, time: Any = False) -> Mapping[Node, Optional[bool]]:
+        evaluated_conditions: Iterable[Node] = self.conditions if condition is None else {condition}
+
+        results: MutableMapping[Node, Optional[bool]] = dict()
+        for phi in evaluated_conditions:
+            results[phi] = phi(trace.project(self.atoms(phi)), dt=dt, time=time) # logic
+
+        return results
+
 
 
 class Trace:
-    values: Mapping[PathType, TimeSeries]
+    values: Dict[PathType, TimeSeries]
 
     def atoms(self) -> Iterable[Atom]:
-        pass
+        return {Atom(k) for k in self.values.keys()}
 
-    def add(self, other: Node) -> Trace:
-        pass
+    def project(self, atoms: Iterable[Atom]) -> Mapping[str: List[(int, Any)]]:
+        return {a.id: [(t, v) for t, v in self.values[a.id]] for a in atoms}
 
-    def __iadd__(self, other: Node) -> Trace:
-        pass
+    @staticmethod
+    def _merge_values(values: List[Optional[TimeSeries]]):
+        n = [i for i in values if i is not None]
+        return n[-1] if n else None
 
-    def update(self, other: Union[Monitor, Trace]) -> None:
-        raise NotImplementedError()
+    def update(self, other: Trace) -> None:
+        for t, s in other.values.items():
+            if t in self.values:
+                self.values[t] = TimeSeries.merge([self.values[t], s], operation=self._merge_values)
+            else:
+                self.values[t] = TimeSeries(s)
 
-    def __ior__(self, other: Union[Monitor, Trace]) -> Trace:
-        pass
+    def __ior__(self, other: Trace) -> Trace:
+        return self | other
 
-    def __or__(self, other: Union[Monitor, Trace]) -> Trace:
-        pass
+    def __or__(self, other:  Trace) -> Trace:
+        result = Trace()
+        result.update(self)
+        result.update(other)
+        return result
 
     @classmethod
-    def _extract_properties(
-            cls, element: Any, prefix: PathType = ()
-    ) -> Iterable[Tuple[PathType, Any]]:
+    def _extract_atom_values(cls, element: Any, prefix: PathType = ()) -> Iterable[Tuple[PathType, Any]]:
         """Convert nested structure into flat list with tuple capturing nested paths."""
         if isinstance(element, dict):
             return funcy.cat(
-                cls._extract_properties(v, prefix + (k,)) for k, v in element.items()
+                cls._extract_atom_values(v, prefix + (k,)) for k, v in element.items()
             )
         if isinstance(element, list):
             return funcy.cat(
-                cls._extract_properties(v, prefix + (str(i),))
+                cls._extract_atom_values(v, prefix + (str(i),))
                 for i, v in enumerate(element)
             )
         return [(prefix, element)]
 
-    def _record(
-            self, element: Any, timestamp: Callable[[Any], int]
-    ) -> None:
+    def _record(self, element: Any, timestamp: Callable[[Any], int]) -> None:
         time = timestamp(element)
         if time is None:
             return
-        for path, value in self._extract_properties(element):
+        for path, value in self._extract_atom_values(element):
             path @= (time, value)
 
-    def record(
-            self, element: Any, *, timestamp: Callable[[Mapping], int]
-    ) -> None:
+    def record(self, element: Any, *, timestamp: Callable[[Mapping], int]) -> None:
         if isinstance(element, dict):
             self._record(element, timestamp)
         else:
             for e in element:
                 self.record(e, timestamp=timestamp)
-
-    def evaluate(
-            self, phi: Union[Node], at: Any = False
-    ) -> Optional[bool]:
-        pass
 
