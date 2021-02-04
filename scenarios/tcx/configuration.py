c91f54009ae7af74769e16ba77339ff458721e3f
Benjamin <benjamin.lesage@york.ac.uk>
2021-02-04 15:32:04 +0000

    Add configuration entry for tool/assembly/cobot position

--- scenarios/tcx/configuration.py
+++ scenarios/tcx/configuration.py
@@ -17,10 +17,25 @@ class Vector3D:
 
 
 @dataclasses.dataclass
-class Operator:
+class Entity:
     position: Vector3D = dataclasses.field(default_factory=Vector3D)
+    rotation: Vector3D = dataclasses.field(default_factory=Vector3D)
 
-    _encoded_fieldnames = {"position": "Entity.position"}
+    _encoded_fieldnames = {
+        "position": "Entity.position",
+        "rotation": "Entity.eulerAngles",
+    }
+
+
+@dataclasses.dataclass
+class Operator(Entity):
+    height: float = dataclasses.field(default=1.75)
+
+    _encoded_fieldnames = {
+        "height": "Operator.height",
+        "position": "Entity.position",
+        "rotation": "Entity.eulerAngles",
+    }
 
 
 @dataclasses.dataclass
@@ -29,10 +44,51 @@ class WorldData:
         default_factory=lambda: datetime.datetime.now()
     )
     operator: Operator = dataclasses.field(default_factory=Operator)
+    cobot: Entity = dataclasses.field(default_factory=Entity)
+    tool: Entity = dataclasses.field(default_factory=Entity)
+    assembly: Entity = dataclasses.field(default_factory=Entity)
     version: str = "0.0.0.1"
 
     _encoded_fieldnames = {
         "operator": "Tim-Operator",
+        "cobot": "ur10-cobot",
+        "tool": "Spot Welder Assembly-welder",
+        "assembly": "TT7302-mandrel-assembly",
         "timestamp": "$Generated",
         "version": "$version",
     }
+
+
+def default() -> WorldData:
+    world = WorldData()
+    #
+    world.operator.position.x = 1.5  # U/D
+    world.operator.position.y = 0  # Height
+    world.operator.position.z = 0  # L/R
+    #
+    world.operator.rotation.x = 0  # Back/Front
+    world.operator.rotation.y = -90  # Direction facing
+    world.operator.rotation.z = 0  # Left / Right
+    #
+    world.operator.height = 1.75
+    #
+    world.cobot.position.x = -0.908
+    world.cobot.position.y = 0.778844
+    world.cobot.position.z = 0.7
+    #
+    world.cobot.rotation.x = 0.0
+    world.cobot.rotation.y = 180.0
+    world.cobot.rotation.z = 0.0
+    #
+    world.tool.position.x = -2.097
+    world.tool.position.y = 0.0
+    world.tool.position.z = 0.0
+    #
+    world.assembly.position.x = -0.105
+    world.assembly.position.y = 0.0  # 0.84
+    world.assembly.position.z = 0.0
+    #
+    world.assembly.rotation.x = 0.0
+    world.assembly.rotation.y = 0.0
+    world.assembly.rotation.z = 90.0
+    return world
