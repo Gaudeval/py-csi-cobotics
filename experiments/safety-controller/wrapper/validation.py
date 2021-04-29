6532d94ec2ce76721b76b7e7f3c1e5a465f97621
Benjamin <benjamin.lesage@york.ac.uk>
2021-04-30 00:26:13 +0100

    Add conditions to validate HC and HRW risk factors

--- experiments/safety-controller/wrapper/validation.py
+++ experiments/safety-controller/wrapper/validation.py
@@ -20,6 +20,31 @@ HSe = (
     & (SafCtr.rngDet.eq(RngDet.near) | SafCtr.rngDet.eq(RngDet.close))
 )
 
+HCe = (
+    (
+        SafCtr.safmod.eq(SafMod.normal)
+        #        | SafCtr.safmod.eq(SafMod.hguid)
+        #        | SafCtr.safmod.eq(SafMod.ssmon)
+        #        | SafCtr.safmod.eq(SafMod.pflim)
+    )
+    & (
+        SafCtr.ract.eq(Act.exchWrkp)
+        | SafCtr.ract.eq(Act.welding)
+        | SafCtr.wact.eq(Act.welding)
+    )
+    & (SafCtr.rngDet.eq(RngDet.close))
+)
+
+HRWe = (
+    (
+        SafCtr.safmod.eq(SafMod.normal)
+        | SafCtr.safmod.eq(SafMod.hguid)
+        | SafCtr.safmod.eq(SafMod.ssmon)
+    )
+    & (SafCtr.rloc.eq(Loc.sharedTbl))
+    & SafCtr.lgtBar
+)
+
 predicates = [
     SafetyCondition(
         "HS:detected",
@@ -31,19 +56,61 @@ predicates = [
         ),
     ),
     SafetyCondition(
-        "HS:handled:1",
+        "HS:mitigated",
         G(implies(SafCtr.hsp.eq(Phase.act), F(SafCtr.hsp.eq(Phase.mit)))),
     ),
     SafetyCondition(
-        "HS:handled:2",
+        "HS:handled",
         G(implies(SafCtr.hsp.eq(Phase.mit), F(SafCtr.hsp.eq(Phase.inact)))),
     ),
     SafetyCondition(
-        "HS:handled:3",
+        "HS:final",
         F(SafCtr.oloc.eq(None)),  # FIXME F (operator.position = final)
     ),
     SafetyCondition(
-        "HS:handled:4",
+        "HS:not_mis",
         G(~SafCtr.hsp.eq(Phase.mis)),
+    ),
+    SafetyCondition(
+        "HC:detected",
+        G(
+            implies(
+                (HCe & SafCtr.hcp.eq(Phase.inact)),
+                timed_until(HCe, SafCtr.hcp.eq(Phase.act), lo=0.0, hi=0.5),
+            )
+        ),
+    ),
+    SafetyCondition(
+        "HC:mitigated",
+        G(implies(SafCtr.hcp.eq(Phase.act), F(SafCtr.hcp.eq(Phase.mit)))),
+    ),
+    SafetyCondition(
+        "HC:handled",
+        G(implies(SafCtr.hcp.eq(Phase.mit), F(SafCtr.hcp.eq(Phase.inact)))),
+    ),
+    SafetyCondition(
+        "HC:not_mis",
+        G(~SafCtr.hcp.eq(Phase.mis)),
+    ),
+    SafetyCondition(
+        "HRW:detected",
+        G(
+            implies(
+                (HRWe & SafCtr.hrwp.eq(Phase.inact)),
+                timed_until(HRWe, SafCtr.hrwp.eq(Phase.act), lo=0.0, hi=0.5),
+            )
+        ),
+    ),
+    SafetyCondition(
+        "HRW:mitigated",
+        G(implies(SafCtr.hrwp.eq(Phase.act), F(SafCtr.hrwp.eq(Phase.mit)))),
+    ),
+    SafetyCondition(
+        "HRW:handled",
+        G(implies(SafCtr.hrwp.eq(Phase.mit), F(SafCtr.hrwp.eq(Phase.inact)))),
+    ),
+    SafetyCondition(
+        "HRW:not_mis",
+        G(~SafCtr.hrwp.eq(Phase.mis)),
     ),
 ]
