9d3340997606efb19fdf14b3782f934187076a64
Gaudeval <m.aqpl.c@gmail.com>
2021-07-30 23:41:56 +0100

    Prototype predicate coverage computation using the dataset library

--- experiments/safecomp-revised/serialisation_dataset.py
+++ experiments/safecomp-revised/serialisation_dataset.py
@@ -1,6 +1,8 @@
 import dataset
 import pickle
 import timeit
+
+import mtl.connective
 import tqdm
 
 from pathlib import Path
@@ -9,14 +11,17 @@ from typing import Any, Iterator
 
 from csi.coverage import Domain
 from csi.experiment import Repository, Run
-from csi.monitor import Trace
-from csi.safety import Atom
+from csi.monitor import Trace, Monitor
+from csi.safety import Atom, Node, SafetyCondition
 
 from wrapper.runner import SafecompControllerRunner
 from wrapper.utils import as_working_directory
 
+default_runner = SafecompControllerRunner(".", None)
 
-domain: dict[Atom, Domain] = SafecompControllerRunner.initialise_registry(None).domain
+domain: dict[Atom, Domain] = default_runner.initialise_registry().domain
+conditions: list[SafetyCondition] = default_runner.safety_conditions
+predicates: set[Node] = default_runner.extract_boolean_predicates(conditions)
 
 
 def collect_states(trace: Trace) -> Iterator[tuple[float, set[tuple[str, Any]]]]:
@@ -24,10 +29,37 @@ def collect_states(trace: Trace) -> Iterator[tuple[float, set[tuple[str, Any]]]]
     events: TimeSeries = TimeSeries.merge([trace.values[e.id] for e in event_keys])
     events.compact()
     t: float
+    # TODO Filter values used in monitor, e.g. ignore unused atoms in any safety condition
     for t, v in events.items():
         s = set()
         for (e, d), i in zip(sorted(domain.items(), key=lambda d: d[0].id), v):
             s.add(("_".join(e.id), d.value(i)))
+        yield t, s
+
+
+def collect_predicates(trace: Trace):
+    # -> Iterator[tuple[float, set[tuple[str, bool]]]]:
+    predicate_keys = sorted(predicates, key=lambda d: str(d))
+    predicate_values = list()
+    for predicate in predicate_keys:
+        v = Monitor().evaluate(
+            trace,
+            predicate,
+            dt=0.01,
+            quantitative=True,
+            logic=mtl.connective.zadeh,
+            time=None,
+        )
+        v = [(t, c >= mtl.connective.zadeh.const_true) for t, c in v]
+        ts: TimeSeries = TimeSeries(v)
+        ts.compact()
+        predicate_values.append(ts)
+    #
+    events: TimeSeries = TimeSeries.merge(predicate_values)
+    for t, v in events.items():
+        s = set()
+        for e, i in zip(predicate_keys, v):
+            s.add((e, i))
         yield t, s
 
 
@@ -45,24 +77,50 @@ def collect_traces(repository_path: Path) -> Iterator[tuple[Run, Trace]]:
 
 if __name__ == "__main__":
     #
-    test_insert: bool = False
+    test_insert: bool = True
     test_coverage: bool = True
     repository = Path(
         "C:\\Users\\Benjamin\\Data\\repositories\\safecomp-revised\\runs.ran"
     )
+    # repository = Path("runs")
     #
     db: dataset.Database
-    states: dataset.Table
+    states_table: dataset.Table
+    predicates_table: dataset.Table
     #
     db = dataset.connect("sqlite:///dataset.test.db")
-    states = db["states"]
+    states_table = db["states"]
+    predicates_table = db["predicates"]
+    pred_value_table = db["predicates_values"]
     #
     if test_insert:
         timer_start = timeit.default_timer()
+        # Assign unique id to predicates
+        predicate_ids: dict[Node, int] = dict()
+        for predicate in predicates:
+            row = predicates_table.find_one(predicate=str(predicate))
+            if row is not None:
+                predicate_ids[predicate] = row["id"]
+            else:
+                i = predicates_table.insert_ignore(
+                    {"predicate": str(predicate)},
+                    ["predicate"],
+                    ensure=True,
+                )
+                predicate_ids[predicate] = i
+        db.commit()
+        #
         for run, trace in tqdm.tqdm(collect_traces(repository)):
             for timestamp, state in collect_states(trace):
                 meta = {"run": str(run.uuid), "timestamp": timestamp}
-                states.insert(dict(state) | meta, ensure=True)
+                states_table.insert(dict(state) | meta, ensure=True)
+            db.commit()
+            #
+            for timestamp, state in collect_predicates(trace):
+                meta = {"run": str(run.uuid), "timestamp": timestamp}
+                pred_value_table.insert(
+                    {str(predicate_ids[p]): v for p, v in state} | meta, ensure=True
+                )
             db.commit()
         timer_end = timeit.default_timer()
         print(f"Insertion duration: {timer_end - timer_start}")
@@ -73,13 +131,27 @@ if __name__ == "__main__":
         atoms_covered = 0
         atoms_count = sum(len(d) for d in domain_columns.values())
         for atom_column in sorted(domain_columns):
-            atom_covered = sum(1 for _ in states.distinct(atom_column))
+            atom_covered = sum(1 for _ in states_table.distinct(atom_column))
             print(
                 f"Atom '{atom_column}': {atom_covered} / {len(domain_columns[atom_column])}"
             )
             atoms_covered += atom_covered
+        # Predicate coverage
+        predicate_columns = {str(i + 1) for i in range(len(predicates))}
+        predicates_covered = 0
+        predicates_count = 2 * len(predicates)
+        for predicate_column in sorted(predicate_columns):
+            predicate_covered = sum(
+                1 for _ in pred_value_table.distinct(predicate_column)
+            )
+            predicate = predicates_table.find_one(id=int(predicate_column))["predicate"]
+            print(f"Predicate '{predicate}': {predicate_covered} / 2")
+            predicates_covered += predicate_covered
+        # TODO Condition/Safety coverage
         timer_end = timeit.default_timer()
         print(f"Atom coverage: {atoms_covered / atoms_count}")
         print(f"               {atoms_covered} / {atoms_count}")
+        print(f"Pred coverage: {predicates_covered / predicates_count}")
+        print(f"               {predicates_covered} / {predicates_count}")
         print(f"     duration: {timer_end - timer_start}")
     db.close()
