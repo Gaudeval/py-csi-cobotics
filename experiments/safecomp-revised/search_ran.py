f304c53890ab3f03f9fdcaf5733246767ed0f0a9
Gaudeval <m.aqpl.c@gmail.com>
2021-11-02 09:45:37 +0000

    Run randomised search runs in parallel

--- experiments/safecomp-revised/search_ran.py
+++ experiments/safecomp-revised/search_ran.py
@@ -1,23 +1,31 @@
 import shutil
 import time
 from pathlib import Path
+from multiprocessing import Pool
 
 
 from wrapper.fitness import RunnerFitnessWrapper
 import random
 
 
+def run(seed):
+    runs = Path("./runs/")
+    w = RunnerFitnessWrapper("./build/", runs, "zadeh", with_features=True)
+    random.seed(seed)
+    X = [random.random() for _ in range(5)]
+    w(X)
+    return seed
+
+
 if __name__ == "__main__":
     runs = Path("./runs/")
-    if runs.exists():
-        shutil.rmtree(runs)
+    # if runs.exists():
+    # shutil.rmtree(runs)
     #
-    w = RunnerFitnessWrapper("./build/", runs, "zadeh", with_features=True)
-
     random.seed(42)
     time_start = time.time()
-    for i in range(100):
-        X = [random.random() for _ in range(5)]
-        w(X)
+    with Pool(processes=10) as pool:
+        for i in pool.imap_unordered(run, range(1001, 10000)):
+            print(i)
     time_end = time.time()
     print(f"Runtime: {time_end - time_start}s")
