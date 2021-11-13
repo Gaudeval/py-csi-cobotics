f145cac341f69adebfa370f6bd641c345f2f5743
Gaudeval <m.aqpl.c@gmail.com>
2021-11-13 16:16:57 +0000

    Add documentation for safety condition definitions
    Fix invalid type declaration for conditions

--- csi/safety.py
+++ csi/safety.py
@@ -1,25 +1,33 @@
+"""
+A definition of safety-related conditions and their components.
+"""
+import typing
 import attr
-
-from typing import TypeVar
 
 from mtfl.ast import AtomicPred, And, Or, Lt, Eq, G, WeakUntil, Implies, Neg, Next
 
 Atom = AtomicPred
 
-Node = TypeVar("Node", AtomicPred, And, Or, Lt, Eq, G, WeakUntil, Implies, Neg, Next)
+Node = typing.Union[AtomicPred, And, Or, Lt, Eq, G, WeakUntil, Implies, Neg, Next]
 
 
 @attr.s(frozen=True, auto_attribs=True, slots=True, hash=True)
 class SafetyCondition:
+    """Uniquely identified safety condition and identifying MTL condition"""
+
     uid: str
     condition: Node
 
 
 @attr.s(frozen=True, auto_attribs=True, slots=True, hash=True)
 class UnsafeControlAction(SafetyCondition):
+    """STPA Unsafe Control Action definition"""
+
     description: str
 
 
 @attr.s(frozen=True, auto_attribs=True, slots=True, hash=True)
 class Hazard(SafetyCondition):
+    """STPA Hazard definition"""
+
     description: str
