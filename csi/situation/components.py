6acee9e8a5e604afa5149119b08b48998fc687a7
Gaudeval <m.aqpl.c@gmail.com>
2021-11-24 09:42:13 +0000

    Clarify typing and implementation for base components definitions

--- csi/situation/components.py
+++ csi/situation/components.py
@@ -9,7 +9,8 @@ As an example a situation where the operator is close to an active welder can
 be built upon the distance between the operator and the welder, and the active
 status of the welder.
 """
-from typing import Optional, Tuple, Union
+from __future__ import annotations
+from typing import Optional, Tuple, Union, Set, TypeVar, Type, overload
 
 import attr
 import lenses
@@ -18,10 +19,15 @@ from mtfl.ast import Or, Lt, Eq
 
 from csi.situation.domain import Domain
 
+PathType = Tuple[str, ...]
+
+C = TypeVar("C", bound="Context")
+
 
 @attr.s(frozen=True, auto_attribs=True, repr=False, slots=True, order=False, init=False)
 class Atom(AtomicPred):
-    path: Tuple[str]
+    """A single component used in the definition of a situation"""
+    path: PathType
     domain: Domain
 
     def __init__(self, path, domain=None):
@@ -36,64 +42,64 @@ class Atom(AtomicPred):
 # TODO Check for duplicate definition
 Node = Union[Atom, And, Or, Lt, Eq, G, WeakUntil, Implies, Neg, Next]
 
-PathType = Tuple[str, ...]
 
-
-@attr.s(
-    auto_attribs=True,
-    repr=True,
-    slots=True,
-    eq=True,
-    order=True,
-    hash=True,
-)
+@attr.s(frozen=True, repr=True, eq=True, order=True, hash=True)
 class Context:
-    path: PathType = attr.ib(tuple())
+    """Acts as a namespace to organise atoms"""
+    _name: str = attr.ib(factory=str)
+    _path: PathType = attr.ib(factory=tuple)
 
-    def __set_name__(self, owner, name):
-        self.name = name
+    def __set_name__(self, owner: Type[Context], name: str) -> None:
+        object.__setattr__(self, "_name", name)
 
-    def __get__(self, instance, owner):
-        return self.__build(getattr(instance, "path", tuple()) + (self.name,))
+    @overload
+    def __get__(self: C, instance: None, owner: None) -> C:
+        ...
 
-    @classmethod
-    def __build(cls, path):
-        return cls(path)
+    @overload
+    def __get__(self: C, instance: Context, owner: Type[Context]) -> C:
+        # instance could really be an object and owner is an Type[object]
+        ...
+
+    def __get__(self: C, instance: Context | None, owner: Type[Context] | None) -> C:
+        if instance is None:
+            return attr.evolve(self, path=(self._name,))
+        return attr.evolve(self, path=instance._path + (self._name,))
 
 
-@attr.s(
-    auto_attribs=True,
-    repr=True,
-    slots=True,
-    eq=True,
-    order=True,
-    hash=True,
-)
+@attr.s(frozen=True, repr=True, eq=True, order=True, hash=True)
 class Alias:
-    condition: Node
+    """Defines a situation within a specific context"""
+    condition: Node = attr.ib()
 
-    def __get__(self, instance, owner):
-        path = getattr(instance, "path", tuple())
+    def __get__(self, instance: Context | None, owner: Type[Context] | None) -> Node:
+        if instance is None:
+            path = tuple()
+        else:
+            path = instance._path
         atoms = set(lenses.bind(self.condition.walk()).Each().Instance(Atom).collect())
         v = {a.id: Atom(path + a.path, a.domain) for a in atoms}
         return self.condition[v]
 
 
-@attr.s(
-    auto_attribs=True,
-    repr=True,
-    slots=True,
-    eq=True,
-    order=True,
-    hash=True,
-)
+@attr.s(frozen=True, repr=True, eq=True, order=True, hash=True)
 class Term:
-    domain: Optional[Domain] = attr.ib(None)
-    _name: str = attr.ib(init=False)
+    """Defines a component in its context"""
+    domain: Optional[Domain] = attr.ib(default=None)
+    _name: str = attr.ib(factory=str)
 
-    def __set_name__(self, owner, name):
-        self._name = name
+    def __set_name__(self, owner: Type[Context], name: str) -> None:
+        object.__setattr__(self, "_name", name)
 
-    def __get__(self, instance, owner):
-        return Atom(getattr(instance, "path", tuple()) + (self._name,), self.domain)
+    @overload
+    def __get__(self, instance: None, owner: None) -> Atom:
+        ...
 
+    @overload
+    def __get__(self, instance: Context, owner: Type[Context]) -> Atom:
+        ...
+
+    def __get__(self, instance: Context | None, owner: Type[Context] | None) -> Atom:
+        if instance is None:
+            return Atom((self._name,), self.domain)
+        return Atom(instance._path + (self._name,), self.domain)
